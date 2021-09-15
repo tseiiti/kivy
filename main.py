c7a26b6f57915a94dfb12e4b8b84ac51354d107c
Seiiti Taniguchi <tseiiti@gmail.com>
2021-09-15 20:45:53 -0300

    requirements do kivy

---
+++ main.py
@@ -0,0 +1,40 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.core.window import Window
+
+class Gerenciador(ScreenManager):
+  pass
+
+class Menu(Screen):
+  pass
+
+class Tarefas(Screen):
+  def __init__(self, tarefas = [], **kwargs):
+    super().__init__(**kwargs)
+    for tarefa in tarefas:
+      self.ids.box.add_widget(Tarefa(text = tarefa))
+  def addWidget(self):
+    texto = self.ids.texto.text
+    self.ids.box.add_widget(Tarefa(text = texto))
+    self.ids.texto.text = ''
+  def on_pre_enter(self):
+    Window.bind(on_keyboard = self.voltar)
+  def on_pre_leave(self):
+    Window.unbind(on_keyboard = self.voltar)
+  def voltar(self, window, key, *args):
+    if key == 27:
+      App.get_running_app().root.current = 'menu'
+    return True
+
+class Tarefa(BoxLayout):
+  def __init__(self, text = '', **kwargs):
+      super().__init__(**kwargs)
+      self.ids.label.text = text
+
+class Test(App):
+  def build(self):
+    return Gerenciador()
+
+if __name__ == '__main__':
+  Test().run()
